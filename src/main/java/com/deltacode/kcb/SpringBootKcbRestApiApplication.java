6c3d53da0932039f80969e1f4772b8ffaf2e0ef6
david-lab17 <davidmakuba@yahoo.com>
2022-09-06 15:06:47 +0300

    add roles

--- src/main/java/com/deltacode/kcb/SpringBootKcbRestApiApplication.java
+++ src/main/java/com/deltacode/kcb/SpringBootKcbRestApiApplication.java
@@ -1,6 +1,9 @@
 package com.deltacode.kcb;
 
+import com.deltacode.kcb.entity.Role;
+import com.deltacode.kcb.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SpringBootKcbRestApiApplication {
- 	@Bean
+public class SpringBootKcbRestApiApplication implements CommandLineRunner {
+	public SpringBootKcbRestApiApplication(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
+
+	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
@@ -19,18 +26,18 @@ public class SpringBootKcbRestApiApplication {
 
 	}
 
-//	@Autowired
-//	private  RoleRepository roleRepository;
-//	@Override
-//	public void run(String... args) throws Exception {
-//		Role adminRole=new Role();
-//		adminRole.setName("ROLE_ADMIN");
-//		roleRepository.save(adminRole);
-//
-//		Role userRole=new Role();
-//		userRole.setName("ROLE_USER");
-//		roleRepository.save(userRole);
-//
-//	}
+
+	private final RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole=new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole=new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
 
